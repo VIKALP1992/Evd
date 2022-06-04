9ce1dd30f7ebf8a9308c37eb59ae1d30cef5062c
vikalp vaibhav mishra <vikalpvaibhav92@gmail.com>
2022-06-04 17:05:00 +0530

    make it better

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'rest_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'coreconew',
+        'NAME': 'coreco',
         'USER':'postgres',
         'PASSWORD':'root',
     }
@@ -132,4 +132,4 @@ REST_FRAMEWORK = {
     ]
 }
 import dj_database_url
-DATABASES['coreconew'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['coreco'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
